064a242892038c6ce8039f7ecbd715d9047cb574
Kamil Trzcinski <ayufan@ayufan.eu>
2016-10-24 15:26:13 +0200

    Enable SetupWraith

--- atv-vendor.mk
+++ atv-vendor.mk
@@ -37,7 +37,8 @@ PRODUCT_PACKAGES += \
 		Katniss \
 		AtvWidget \
 		YouTubeLeanback \
-		LandscapeWallpaper
+		LandscapeWallpaper \
+		SetupWraith
 
 ifeq ($(GAPPS_FORCE_WEBVIEW_OVERRIDES),true)
 ifneq ($(filter-out $(call get-allowed-api-levels),24),)
