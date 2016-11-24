3fe93da8bdbfcfffd8cd62f4778c026509f64e34
Kamil Trzcinski <ayufan@ayufan.eu>
2016-11-24 20:31:21 +0100

    Add support for Marshmallow and allow to exclude packages

--- atv-vendor.mk
+++ atv-vendor.mk
@@ -3,16 +3,12 @@ include $(GAPPS_FILES)
 
 DEVICE_PACKAGE_OVERLAYS += $(GAPPS_DEVICE_FILES_PATH)/overlay/pico
 
-PRODUCT_PACKAGES += \
+GAPPS_PRODUCT_PACKAGES += \
 		BugReportSender \
 		ConfigUpdater \
 		GoogleBackupTransport \
 		GoogleContactsSyncAdapter \
-		GoogleExtServices \
-		GoogleExtShared \
-		GooglePackageInstaller \
 		GoogleServicesFramework \
-		GoogleTTS \
 		NoTouchAuthDelegate \
 		PrebuiltGmsCorePano \
 		Tubesky \
@@ -33,11 +29,31 @@ PRODUCT_PACKAGES += \
 		CanvasPackageInstaller \
 		FuguPairingTutorial \
 		PlayGames \
-		AtvRemoteService \
 		Katniss \
 		AtvWidget \
 		YouTubeLeanback \
-		LandscapeWallpaper
+		LandscapeWallpaper \
+		SetupWraith
+
+ifneq ($(filter $(call get-allowed-api-levels),23),)
+GAPPS_PRODUCT_PACKAGES += \
+    GoogleTTS \
+    GooglePackageInstaller
+endif
+
+ifneq ($(filter $(call get-allowed-api-levels),24),)
+GAPPS_PRODUCT_PACKAGES += \
+    GooglePrintRecommendationService \
+    GoogleExtServices \
+    GoogleExtShared \
+    AtvRemoteService
+endif
+
+# This needs to be at the end of standard files, but before the GAPPS_FORCE_* options,
+# since those also affect DEVICE_PACKAGE_OVERLAYS. We don't want to exclude a package
+# that also has an overlay, since that will make us use the overlay but not have the
+# package. This can cause issues.
+PRODUCT_PACKAGES += $(filter-out $(GAPPS_EXCLUDED_PACKAGES),$(GAPPS_PRODUCT_PACKAGES))
 
 ifeq ($(GAPPS_FORCE_WEBVIEW_OVERRIDES),true)
 ifneq ($(filter-out $(call get-allowed-api-levels),24),)
