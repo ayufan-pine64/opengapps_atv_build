5f231a69801ceb542185f1cb7b9207b4ce232487
Kamil Trzciński <ayufan@ayufan.eu>
2016-12-04 15:17:12 +0100

    Fix prebuilt libatv_uinputbridge
    It seems that `LOCAL_MULTILIB := both` is broken on Android 6.0

--- modules/AtvRemoteService/Android.mk
+++ modules/AtvRemoteService/Android.mk
@@ -15,7 +15,15 @@ include $(BUILD_GAPPS_PREBUILT_APK)
 include $(CLEAR_VARS)
 include $(GAPPS_CLEAR_VARS)
 LOCAL_MODULE := libatv_uinputbridge
+full_name := $(LOCAL_MODULE)$(LOCAL_MODULE_SUFFIX)
 ifdef TARGET_2ND_ARCH
-  LOCAL_MULTILIB := both
+  LOCAL_SRC_FILES := $(call gapps-find-lib-for-arch,$(TARGET_2ND_ARCH),$(lib_prefix)lib,$(full_name))
+  LOCAL_MODULE_PATH := $($(TARGET_2ND_ARCH_VAR_PREFIX)TARGET_OUT_SHARED_LIBRARIES)
+else
+  LOCAL_SRC_FILES := $(call gapps-find-lib-for-arch,$(TARGET_ARCH),$(lib_prefix)lib,$(full_name))
+  LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
 endif
-include $(BUILD_GAPPS_PREBUILT_SHARED_LIBRARY)
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := $(TARGET_SHLIB_SUFFIX)
+include $(BUILD_PREBUILT)
